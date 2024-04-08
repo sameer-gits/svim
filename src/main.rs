66a2cfe7181994f2306bcf74ed012bebc2396cd6
sameer-gits <ms3054825@gmail.com>
2024-04-08 22:18:48 +0530

    Terminal is clearing now

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
+use crossterm::cursor::MoveTo;
+use crossterm::{
+    terminal::{self, Clear, ClearType},
+    QueueableCommand,
+};
+use std::io::{stdout, Write};
+use std::thread;
+use std::time::Duration;
+
 fn main() {
-    println!("Hello, world!");
+    let mut stdout = stdout();
+
+    terminal::enable_raw_mode().unwrap();
+    stdout.queue(Clear(ClearType::All)).unwrap();
+    stdout.queue(Clear(ClearType::Purge)).unwrap();
+    stdout.queue(MoveTo(5, 5)).unwrap();
+    stdout.flush().unwrap();
+    thread::sleep(Duration::from_secs(5));
+    terminal::disable_raw_mode().unwrap();
 }
