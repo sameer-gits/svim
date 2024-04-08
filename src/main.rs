e9c4f94c3122d87792616987c42c26d31e9518e6
sameer-gits <ms3054825@gmail.com>
2024-04-08 23:42:01 +0530

    clean the code

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,24 @@
 use crossterm::cursor::MoveTo;
 use crossterm::{
-    terminal::{self, EnterAlternateScreen, LeaveAlternateScreen},
+    terminal::{size, EnterAlternateScreen, LeaveAlternateScreen, enable_raw_mode, disable_raw_mode},
     QueueableCommand,
 };
 use std::io::{stdout, Write};
-use std::thread;
+use std::thread::sleep;
 use std::time::Duration;
 
 fn main() {
     let mut stdout = stdout();
     let tildr = b"~";
-    let (_, h) = terminal::size().unwrap();
+    let (_, h) = size().unwrap();
 
-    terminal::enable_raw_mode().unwrap();
+    enable_raw_mode().unwrap();
     stdout.queue(EnterAlternateScreen).unwrap();
     stdout.queue(MoveTo(0, 0)).unwrap();
     stdout.write(&tildr.repeat(h as usize)).unwrap();
     print!("{}", h);
     stdout.flush().unwrap();
-    thread::sleep(Duration::from_secs(5));
+    sleep(Duration::from_secs(5));
     stdout.queue(LeaveAlternateScreen).unwrap();
-    terminal::disable_raw_mode().unwrap();
+    disable_raw_mode().unwrap();
 }
