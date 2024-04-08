a1a581f85a8b344b134b40e8becc0e52fe1deb0f
sameer-gits <ms3054825@gmail.com>
2024-04-08 22:46:17 +0530

    removed clearing fn now Entering new screen buffer and Leaving new screen buffer

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 use crossterm::cursor::MoveTo;
 use crossterm::{
-    terminal::{self, Clear, ClearType},
+    terminal::{self, EnterAlternateScreen, LeaveAlternateScreen},
     QueueableCommand,
 };
 use std::io::{stdout, Write};
@@ -10,11 +10,13 @@ use std::time::Duration;
 fn main() {
     let mut stdout = stdout();
 
+    //let (w, h) = terminal::size().unwrap();
+
     terminal::enable_raw_mode().unwrap();
-    stdout.queue(Clear(ClearType::All)).unwrap();
-    stdout.queue(Clear(ClearType::Purge)).unwrap();
+    stdout.queue(EnterAlternateScreen).unwrap();
     stdout.queue(MoveTo(5, 5)).unwrap();
     stdout.flush().unwrap();
     thread::sleep(Duration::from_secs(5));
+    stdout.queue(LeaveAlternateScreen).unwrap();
     terminal::disable_raw_mode().unwrap();
 }
