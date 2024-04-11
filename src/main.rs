0c7f6cc09a2b468dd881f6c8332f0d4abb1dbb88
sameer-gits <ms3054825@gmail.com>
2024-04-11 16:24:36 +0530

    saving cursor positon and restoring it

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,6 @@
-use crossterm::cursor::{MoveDown, MoveLeft, MoveRight, MoveTo, MoveUp};
+use crossterm::cursor::{
+    MoveDown, MoveLeft, MoveRight, MoveTo, MoveUp, RestorePosition, SavePosition,
+};
 use crossterm::event::{self, Event, KeyCode, KeyEvent, KeyModifiers};
 use crossterm::{
     terminal::{
@@ -12,11 +14,14 @@ fn main() -> Result<()> {
     let mut stdout = stdout();
     enable_raw_mode()?;
     stdout.queue(EnterAlternateScreen)?;
-
-    let terminal_size = size()?;
-    print_tilde(&mut stdout, terminal_size)?;
+    stdout.queue(MoveTo(4, 0))?;
+    stdout.flush()?;
     loop {
         match read_char()? {
+            'r' => {
+                let terminal_size = size()?;
+                print_tilde(&mut stdout, terminal_size)?;
+            }
             'q' => {
                 quit_svim(&mut stdout)?;
                 break;
@@ -70,13 +75,14 @@ fn read_char() -> Result<char> {
 
 fn print_tilde(stdout: &mut std::io::Stdout, terminal_size: (u16, u16)) -> Result<()> {
     let tilde = b"~";
+    stdout.queue(SavePosition)?;
 
     for i in 0..terminal_size.1 - 2 {
         stdout.queue(MoveTo(0, i))?;
         stdout.write_all(tilde)?;
         print!("{}", terminal_size.1);
     }
-    stdout.queue(MoveTo(4, 0))?;
+    stdout.queue(RestorePosition)?;
     stdout.flush()?;
 
     Ok(())
